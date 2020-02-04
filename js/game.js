75fd437182687a89219acc43a59f8a6e50e661a6
Julianj22 <jacklin@aclanyc.com>
2020-02-04 23:18:54 +0000

    done

--- js/game.js
+++ js/game.js
@@ -6,24 +6,24 @@ var game = {
     levels: {
 
         start: {
-            message: "You come across a dark cave",
+            message: "You wake up, but do you check your phone?",
             choices: [
                 {
-                    text: "Enter the cave",
-                    nextLevel: "cave",
+                    text: "Check phone",
+                    nextLevel: "phonePath1",
                 },
 
                 {
-                    text: "Keep on moving",
-                    nextLevel: "field",
+                    text: "Don't check phone",
+                    nextLevel: "lifePath1",
                 },
             ]
         },
 
-        cave: {
+        disasterEnd: {
             background_image: "fire.gif",
             music: "Final-Fantasy-7-Boss-Battle.mp3",
-            message: "You come across a fire monster or something!",
+            message: "This world has abandoned its people. Humanity is reaching torwards the end.",
             choices: [
                 {
                     text: "Start over",
@@ -32,8 +32,10 @@ var game = {
             ]
         },
 
-        field: {
-            message: "Some adventurer you are...",
+        sleepEnd: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "sleep",
             choices: [
                 {
                     text: "Start over",
@@ -41,6 +43,173 @@ var game = {
                 },
             ]
         },
+
+        awakeEnd: {
+            background_image: "fire.gif",
+            music: "Final-Fantasy-7-Boss-Battle.mp3",
+            message: "You are awoken, and must now join the others. An entirely new world awaits.",
+            choices: [
+                {
+                    text: "Start over",
+                    nextLevel: "start",
+                },
+            ]
+        },
+
+        phonePath1: {
+            message: "C. is the most popular social media program to date. Its goal is to connect people, and all other motives are unknown. After C.'s initial launch in 2079, the app's founder's mysteriously vanished.",
+            choices: [
+                {
+                    text: "Check the news",
+                    nextLevel: "disasterEnd1",
+                },
+                {
+                    text: "Check C.",
+                    nextLevel: "phonePath2",
+                },
+            ]
+        },
+
+        disasterEnd1: {
+            message: "You learn that a viral outbreak taken shape in all major cities across the globe, including yours. Both the infection and mortality rates are now greater that 95%. Noteable side effects include increased aggrivation and instatiable hunger. You living room window has just been shattered. run.",
+            choices: [
+                {
+                    text: "...",
+                    nextLevel: "disasterEnd",
+                },
+                {
+                    text: "...",
+                    nextLevel: "disasterEnd",
+                },
+            ]
+        },
+
+        phonePath2: {
+            message: "You have 1 new notification",
+            choices: [
+                {
+                    text: "Ignore",
+                    nextLevel: "sleepEnd1",
+                },
+                {
+                    text: "Open notification",
+                    nextLevel: "user_8",
+                },
+            ]
+        },
+
+
+        lifePath1: {
+            message: "You are beginning to get out of bed",
+            choices: [
+                {
+                    text: "get out of bed on your right",
+                    nextLevel: "lifePath2",
+                },
+                {
+                    text: "get out of bed on your left",
+                    nextLevel: "lifepath2",
+                },
+            ]
+        },
+
+        lifePath2: {
+            message: "you are brushing your teeth",
+            choices: [
+                {
+                    text: "brush with your left hand",
+                    nextLevel: "lifePath3",
+                },
+                {
+                    text: "brush with your right hand",
+                    nextLevel: "lifePath3",
+                },
+            ]
+        },
+
+        lifePath3: {
+            message: "you are walking torwards the front door",
+            choices: [
+                {
+                    text: "leave the apartment",
+                    nextLevel: "lifePath4",
+                },
+                {
+                    text: "stay",
+                    nextLevel: "lifePath4",
+                },
+            ]
+        },
+
+        lifePath4: {
+            message: "You decided to stay in the apartment today. This is how most of your days progress. You are now sitting on the couch in front of the t.v.",
+            choices: [
+                {
+                    text: "turn on the t.v.",
+                    nextLevel: "disasterEnd2",
+                },
+                {
+                    text: "leave the t.v. off",
+                    nextLevel: "lifePath5",
+                },
+            ]
+        },
+
+        disasterEnd2: {
+            message: "All news channels are reporting a near cataclysmic global market crash with unprecedented effects. You recieve many calls from your place of work. You panic.",
+            choices: [
+                {
+                    text: "...",
+                    nextLevel: "disasterEnd",
+                },
+                {
+                    text: "...",
+                    nextLevel: "disasterEnd",
+                },
+            ]
+        },
+
+        lifePath5: {
+            message: "You hear knocking at the door",
+            choices: [
+                {
+                    text: "answer the door",
+                    nextLevel: "sleep1",
+                },
+                {
+                    text: "stay",
+                    nextLevel: "phonePath2",
+                },
+            ]
+        },
+
+        sleep1: {
+            message: "A man in all black stands before you. He reveals a damp rag and forcefully holds it up to your face. You are defenseless.",
+            choices: [
+                {
+                    text: "...",
+                    nextLevel: "sleepEnd",
+                },
+                {
+                    text: "...",
+                    nextLevel: "sleepEnd",
+                },
+            ]
+        },
+
+        // user_8: {
+        //     message: "",
+        //     choices: [
+        //         {
+        //             text: "",
+        //             nextLevel: "",
+        //         },
+        //         {
+        //             text: "",
+        //             nextLevel: "",
+        //         },
+        //     ]
+        // },
 
     }
 };
